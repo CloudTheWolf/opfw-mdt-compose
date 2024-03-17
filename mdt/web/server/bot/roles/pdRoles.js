9b9e5b2e0d976461fe412e554a2e71313165fdab
CloudTheWolf <ukkmmichael@gmail.com>
2024-03-17 13:10:47 +0000

    Create pdRoles.js

---
+++ mdt/web/server/bot/roles/pdRoles.js
@@ -0,0 +1,84 @@
+export const nonAutomaticRoles = [ 
+    "chief", 
+    "assistant chief", 
+    "deputy chief", 
+    "dyno bot",
+    "legacy bot", 
+    "legacy mdt",
+     "captain" 
+]; 
+    
+export const roles = { 
+    "cadet": "", 
+    "lieutenant": "", 
+    "sergeant first class": "", 
+    "sergeant": "", 
+    "corporal": "", 
+    "senior trooper": "",
+    "trooper first class": "", 
+    "trooper": "", 
+
+    "sheriff": "", 
+    "undersheriff": "", 
+    "bcso lieutenant": "",
+    "bcso sergeant first class": "", 
+    "bcso sergeant": "", 
+    "bcso corporal": "",
+    "senior deputy": "",
+    "deputy first class": "", 
+    "deputy": "",
+    "bcso supervisor": "",
+    "bcso command": "",
+    "game warden - supervisor": "", 
+    "game warden": "", 
+    
+    "sahp comissioner": "",
+    "sahp command": "", 
+    "sahp supervisor": "", 
+    
+    "swat - command": "", 
+    "swat - supervisor": "", 
+    "swat - team leader": "", 
+    "swat - training officer": "", 
+    "swat - member": "", 
+    "swat - recruit": "", 
+    "swat": "", 
+    
+    "ftp - command": "", 
+    "ftp - supervisor": "", 
+    "ftp - senior trainer": "", 
+    "ftp - trainer": "", 
+    "ftp - jr trainer": "",
+    "ftp - recruit": "", 
+    "ftp": "", 
+    
+    "sasp management": "", 
+    
+    "pursuit lead": "",
+    "pursuit cert": "",
+    
+    "sasp": "", 
+    "bcso": "", 
+    "sahp": "", 
+    
+    "bar certified": "", 
+    "assistant district attorney": "", 
+    "district attorney": "", 
+    "attorney general": "", 
+    "judge": "", 
+    "chief justice": "",
+    "doj": "", 
+    "student": "", 
+    
+    "ia - command" : "", 
+    "ia - officer" : "", 
+    
+    "federal investigation bureau": "",
+    
+    "sasp mentor command": "",
+    "sasp senior mentor": "",
+    "sasp mentor": "",
+
+    "pcp": ""
+
+};
